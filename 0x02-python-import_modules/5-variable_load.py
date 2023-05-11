b490bac53b03abd6dd4737261402a9c73f639abc
hibayas <122922487+hibayas@users.noreply.github.com>
2023-05-11 14:35:15 +0100

    t

--- 0x02-python-import_modules/5-variable_load.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-# 5-variable_load.py
-# Rajy Hiba <rajyhiba1@gmail.com>
-
-if __name__ == "__main__":
-    """Print the value of variable a from variable_load_5."""
-    from variable_load_5 import a
-
-    print(a)
